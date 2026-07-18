c62d1dea2c93fa61b84dc19fabaa0e083d28e3b9
rikiniki <ristevskin@gmail.com>
2026-07-18 16:03:29 +0200

    Update image source in app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def sacuvaj_termine(termini):
     st.write("Podaci bi ovde bili sačuvani.")
 
 # --- APLIKACIJA ---
-st.image("_20260716_172456_com_viber_voip_MediaPreviewActivity.jpg", width=300)
+st.image("https://raw.githubusercontent.com/ristevskin-source/Kod-kubanca/main/PreviewActivity.jpg", width=300)
 st.title("Kod Kubanca")
 
 # --- JAVNI DEO ---
